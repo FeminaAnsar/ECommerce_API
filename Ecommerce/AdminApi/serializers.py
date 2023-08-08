fd22d3865e009303a17a4f1ec079672e39dcb3a3
Femina <feminaansar23@gmail.com>
2023-08-09 01:06:22 +0530

    adding Ecommerce

--- Ecommerce/AdminApi/serializers.py
+++ Ecommerce/AdminApi/serializers.py
@@ -27,7 +27,7 @@ class AddProductSerializer(serializers.ModelSerializer):
 
 
 class EditProductSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = Product
         fields = [ 'description','price','stock','available', 'image','offer']
@@ -38,3 +38,34 @@ class DeleteProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
 
+
+class ListCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'category_name']
+
+
+# List Product Serializer
+class ListProductSerializer(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'product_name', 'price', 'offer', 'discount']
+
+    def get_discount(self, obj):
+        discount = obj.price - (obj.price * obj.offer / 100)
+        return discount
+
+
+# Product Details Serializer
+class ProductDetailSerializer(serializers.ModelSerializer):
+    discount = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Product
+        fields = ['id', 'product_name', 'description', 'price', 'stock', 'available', 'image', 'offer', 'discount']
+
+    def get_discount(self, obj):
+        discount = obj.price - (obj.price * obj.offer / 100)
+        return discount
--- Ecommerce/AdminApi/views.py
+++ Ecommerce/AdminApi/views.py
@@ -1,5 +1,65 @@
-from django.shortcuts import render
+from .models import Category, Product
+from rest_framework import generics
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from django_filters import FilterSet, RangeFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
+from .serializers import (CategorySerializer, AddProductSerializer,
+                          EditProductSerializer,DeleteProductSerializer,
+                          ListCategorySerializer,ListProductSerializer,
+                          ProductDetailSerializer)
 
 
+class AddCategoryView(generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
 
+
+class AddProductView(generics.CreateAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = Product.objects.all()
+    serializer_class = AddProductSerializer
+
+
+class EditProductView(generics.UpdateAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = Product.objects.all()
+    serializer_class = EditProductSerializer
+
+
+class DeleteProductView(generics.DestroyAPIView):
+    permission_classes = [IsAdminUser]
+    queryset = Product.objects.all()
+    serializer_class = DeleteProductSerializer
+
+
+class ListCategoryView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Category.objects.all()
+    serializer_class = ListCategorySerializer
+
+
+class PriceFilter(FilterSet):
+    price = RangeFilter()
+    class Meta:
+        model = Product
+        fields = ['price']
+
+# List Product View
+class ListProductView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ListProductSerializer
+
+    filter_backends = [DjangoFilterBackend,OrderingFilter,SearchFilter]
+    filterset_class = PriceFilter
+    search_fields = ['category__category_name','product_name',]
+
+
+# Product Detail View
+class ProductDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
 # Create your views here.
